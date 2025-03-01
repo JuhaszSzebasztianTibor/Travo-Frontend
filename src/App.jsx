da0b928bc4156dc75e965ccf4af1dd43f37319da
JuhaszSzebasztianTibor <juhaszszebasztian@gmail.com>
2025-03-01 15:42:13 +0200

    Added Login and Signup ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout/Layout.jsx";
 import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/AuthPage/Login.jsx";
+import Signup from "./pages/AuthPage/Signup.jsx";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Layout>
     </Router>
